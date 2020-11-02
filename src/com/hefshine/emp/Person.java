3ec937071326221dd8c93752f443b02f1587c0be
santoshi215 <70130604+santoshi215@users.noreply.github.com>
2020-11-02 11:08:42 +0530

    1

---
+++ src/com/hefshine/emp/Person.java
@@ -0,0 +1,20 @@
+package com.hefshine.emp;
+
+public class Person {
+	
+@SuppressWarnings("unused")
+private Address address;
+	
+	
+	public static void main(String[] args)
+	{
+	Address ad=new Address();
+	ad.display();
+			
+			
+	}
+	
+	
+
+
+}
---
+++ src/com/hefshine/emp/Test.java
@@ -0,0 +1,18 @@
+package com.hefshine.emp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+
+	public static void main(String[] args) {		
+		
+		ApplicationContext con=new ClassPathXmlApplicationContext("config.xml");
+		Employee emp=(Employee) con.getBean("e1");
+		emp.work();
+		
+
+	}
+
+}
